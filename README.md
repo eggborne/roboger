df290916a484c06cafbd6cb17fdd86be5332e5bb
Mike Donovan <mike@mikedonovan.dev>
2023-01-20 11:27:52 -0800

    Add business functions and tests

--- README.md
+++ README.md
@@ -36,5 +36,19 @@ Test: "It should return an array of numbers from 0 to the user's inputted number
 Code: getNumberArray(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
 
+Describe: getConvertedNumber()
+
+Test: "It should change a 1 to "Beep!"
+Code: getConvertedNumber(1);
+Expected Output: "Beep!"
+
+Test: "It should change a 2 to "Boop!"
+Code: getConvertedNumber(2);
+Expected Output: "Boop!"
+
+Test: "It should change a 3 to "Won't you be my neighbor?"
+Code: getConvertedNumber(3);
+Expected Output: "Won't you be my neighbor?"
+
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 window.onload = () => {
-  
+
+}
+
+function getNumberArray(num) {
+  let outputArray = [0];
+  for (let i = 1; i <= num; i++) {
+    outputArray.push(i);
+  }
+  return outputArray;
+}
+
+function getConvertedNumber(num) {
+  let numberString = num.toString();
+  let convertedNumber = num;
+  if (numberString.includes('1')) {
+    convertedNumber = 'Beep!';
+  } else if (numberString.includes('2')) {
+    convertedNumber = 'Boop!';
+  } else if (numberString.includes('3')) {
+    convertedNumber = 'Won\'t you be my neighbor?';
+  }
+  return convertedNumber;
 }
